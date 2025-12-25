3c1d200b3c78524af575665862c610f5cc4964d0
Chiranjeev Kashyap <developer.chiranjeev@gmail.com>
2025-12-25 09:54:21 +0530

    spring data jpa interface & dynamic query

---
+++ src/main/java/com/chiranjeevkashyap/springboot/repositories/ProductRepository.java
@@ -0,0 +1,22 @@
+package com.chiranjeevkashyap.springboot.repositories;
+
+import com.chiranjeevkashyap.springboot.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    List<ProductEntity> findByTitle(String s);
+    List<ProductEntity> findByCreatedAtAfter(LocalDateTime of);
+    List<ProductEntity> findByQuantityAndPrice(int i, BigDecimal i1);
+    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
+
+    @Query("select p from ProductEntity p where p.sku = ?1 and p.price = ?2")
+    Optional<ProductEntity> findBySkuAndPrice(String sku, BigDecimal price);
+}
--- src/test/java/com/chiranjeevkashyap/springboot/SpringBootCourseApplicationTests.java
+++ src/test/java/com/chiranjeevkashyap/springboot/SpringBootCourseApplicationTests.java
@@ -1,13 +1,51 @@
 package com.chiranjeevkashyap.springboot;
 
+import com.chiranjeevkashyap.springboot.entities.ProductEntity;
+import com.chiranjeevkashyap.springboot.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class SpringBootCourseApplicationTests {
+	@Autowired
+	ProductRepository productRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testRepository() {
+		ProductEntity productEntity = ProductEntity.builder()
+				.sku("ASH-5KG-RIC")
+				.price(BigDecimal.valueOf(210))
+				.title("Ashirvaad Select Basmati Rice 5kg")
+				.quantity(130)
+				.build();
+		ProductEntity savedEntity = productRepository.save(productEntity);
+		System.out.println(savedEntity);
+	}
+
+	@Test
+	void getRepository() {
+		List<ProductEntity> productEntityList = productRepository.findAll();
+		System.out.println(productEntityList);
+	}
+
+	@Test
+	void findByTest() {
+//		List<ProductEntity> productEntityList = productRepository.findByTitle("Fortune Sunflower Oil 1L");
+//		LocalDateTime localDateTime = LocalDateTime.of(2025, 12, 25, 1, 1, 0);
+//		List<ProductEntity> productEntityList = productRepository.findByCreatedAtAfter(localDateTime);
+//		List<ProductEntity> productEntityList = productRepository.findByQuantityAndPrice(500, BigDecimal.valueOf(15));
+//      Optional<ProductEntity> productEntity = productRepository.findByTitleAndPrice("Lifebuoy Soap 100g", BigDecimal.valueOf(15));
+        Optional<ProductEntity> productEntity = productRepository.findBySkuAndPrice("LIF-100G-SOP", BigDecimal.valueOf(15));
+		productEntity.ifPresent(System.out::println);
+	}
 }
